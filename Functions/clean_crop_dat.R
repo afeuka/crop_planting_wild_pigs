999d95c6859a1aa1c51ed4c67682a941315e0bbf
Abbey Feuka <46827256+afeuka@users.noreply.github.com>
2025-07-15 12:48:29 -0600

    functions initial commit

---
+++ Functions/clean_crop_dat.R
@@ -0,0 +1,102 @@
+### Title: Data cleaning function for crop planting analysis
+### Author: Abbey Feuka
+### Date: 19AUG24
+### Notes:
+
+
+clean_crop_dat <- function(dat_orig, #original data, uncleaned
+                           commod_name, #lower case commodity/crop name ("corn","wheat",etc)
+                           nb=T,
+                           temporal=T,
+                           only_pigs=F #logical, only include counties with pigs
+                           # pig.cov #column name of covariate used for pig inference
+                           ){
+  require(tidyverse)
+  
+  dat <- dat_orig %>% 
+    filter(year<2023 & year >=2009) %>% 
+    filter(commodity_desc==toupper(commod_name)) %>% 
+    filter(!is.nan(plant.anom) & 
+             !is.na(plant.anom) & 
+             !is.na(GEOID) & year>=2009) %>% 
+    distinct() 
+  
+  if(only_pigs){
+    dat <- dat %>% filter(!is.nan(plant.anom) & 
+                            !is.na(plant.anom) & 
+                            !is.na(GEOID)  &
+                            pig.last.year==1) 
+  }
+  
+  #crp interaction
+  dat$crp.nfsp <- dat$prop_nfsp * dat$crp_prop
+  dat$crp.nfsp.sc <- scale(dat$crp.nfsp)
+
+  covs_all <- data.frame(
+    cov=c("plant.anom",
+          "GEOID",
+          "division_grp",
+          # "pig.last.year.sc",
+          "temp5trend.sc",
+          "precip5trend.sc",
+          "reg.roi5trend.sc",
+          if(nb){"plant.anom.nb.sc"},
+          if(temporal){"plant.anom.prev.sc"},
+          # "take5trend.sc",
+          # "take.hog.intens.prev.sc",
+          "take.hog.intens.5yeartrend.sc",
+          "prop.nfsp.sc",
+          "crp.prop.sc",
+          "crp.nfsp.sc"
+    ),
+    name=c("Planting anomaly",
+           "County",
+           "Ecoregion",
+           # "Pig presence previous year",
+           "Temperature 5 yr trend",
+           "Precipitation 5 yr trend",
+           "ROI 5 yr trend",
+           if(nb){"Neighboring planting anomaly"},
+           if(temporal){"Previous year's planting anomaly"},
+           # "Take 5 yr trend",
+           # "Take per hog intensity previous year",
+           "Take per wild pig intensity 5 yr trend",
+           "Prop. of county with wild pigs",
+           "Prop. CRP land",
+           "CRP x pigs"
+    ))
+  # pig_cov_name <- covs_all$name[which(covs_all$cov==paste0(pig_cov,".sc"))]
+  
+  covsx_all <- covs_all[-(1:3),]
+  
+  # if(pig_cov=="ever.pigs" | pig_cov=="nfsp.level" | pig_cov=="hog.intensity"){
+  #   tidx <- which(covsx_all$cov=="take5trend.sc")
+  #   xmat <- xmat[,-tidx]
+  #   covsx_all <- covsx_all[-tidx,]
+  # }
+  
+  dat_clean <- na.omit(dat[,covs_all$cov])
+  dat_clean$county_idx <- as.numeric(factor(dat_clean$GEOID))
+  dat_clean$region_idx <- as.numeric(factor(dat_clean$division_grp))
+  
+  xmat <- dat_clean[,covsx_all$cov]
+  xmat <- sapply(xmat,as.numeric)
+  
+  covsx_all <- covs_all[-(1:3),]
+  
+  dat_clean <- na.omit(dat_clean[,covs_all$cov])
+  dat_clean$county_idx <- as.numeric(factor(dat_clean$GEOID))
+  dat_clean$region_idx <- as.numeric(factor(dat_clean$division_grp))
+  
+  reg_count_idx <- dat_clean %>% group_by(county_idx) %>% 
+    summarise(reg_count_idx=unique(region_idx)) %>% 
+    arrange(county_idx)
+  
+  xmat <- dat_clean[,covsx_all$cov]
+  xmat <- sapply(xmat,as.numeric)
+  
+  return(list(dat_clean=dat_clean,
+         covsx=covsx_all,
+         xmat=xmat,
+         reg_count_idx=reg_count_idx))
+}
---
+++ Functions/fun_response.R
@@ -0,0 +1,133 @@
+### Title: Functional response function for crop planting analysis
+### Author: Abbey Feuka
+### Date: 19AUG24
+### Notes:
+
+# dat_df <- dat_op$dat_clean
+# scale_cov_name <- "crp.nfsp.sc"
+# beta_long <- beta_long_all
+# s_long <- s_long_all
+# covsx <- dat_op$covsx
+# x_incr<-0.5
+# spatial=TRUE
+
+fun_response <- function(dat_df, #dat_clean or dat_clean_op
+                         scale_cov_name, #scaled covariate column name from dat_df
+                         covsx, #covariate matrix, in order fit to modek
+                         beta_long,#pivoted mcmc samples for beta coefficients
+                         s, #samples[,"s"] for spatial random effect
+                         # tau,#vector of mcmc samples for tau regression error
+                         x_incr=0.5,#'by' argument for creating sequence of scaled x's
+                         spatial=FALSE){ #logical to include intercept in calculation (FALSE for spatial RE's)
+  require(tidyverse)
+  
+  dat_df <- as.data.frame(dat_df)
+  anom_cov <- data.frame(sc=seq(range(dat_df[,scale_cov_name])[1],
+                                 range(dat_df[,scale_cov_name])[2],
+                                by=x_incr))
+  anom_cov$bt <- anom_cov$sc *attr(dat_df[,scale_cov_name], 'scaled:scale') + 
+    attr(dat_df[,scale_cov_name], 'scaled:center')
+  
+  crp_cov <- data.frame(sc=seq(range(dat_df[,'crp.prop.sc'])[1],
+                                range(dat_df[,'crp.prop.sc'])[2],
+                                length=length(anom_cov$sc)))
+  crp_cov$bt <- crp_cov$sc *attr(dat_df[,'crp.prop.sc'], 'scaled:scale') + 
+    attr(dat_df[,'crp.prop.sc'], 'scaled:center')
+
+  nfsp_cov <- data.frame(sc=seq(range(dat_df[,'prop.nfsp.sc'])[1],
+                               range(dat_df[,'prop.nfsp.sc'])[2],
+                               length=length(anom_cov$sc)))
+  nfsp_cov$bt <- nfsp_cov$sc *attr(dat_df[,'prop.nfsp.sc'], 'scaled:scale') + 
+    attr(dat_df[,'prop.nfsp.sc'], 'scaled:center')
+  
+  beta_cov <- beta_long %>% filter(cov==covsx$name[covsx$cov==scale_cov_name]) %>% select(value)
+  beta_cov <- unlist(c(beta_cov),use.names = F)
+  
+  beta_crp <- beta_long %>% filter(cov==covsx$name[covsx$cov=='crp.prop.sc']) %>% select(value)
+  beta_crp <- unlist(c(beta_crp),use.names = F)
+  
+  beta_nfsp <- beta_long %>% filter(cov==covsx$name[covsx$cov=='prop.nfsp.sc']) %>% select(value)
+  beta_nfsp <- unlist(c(beta_nfsp),use.names = F)
+  
+  ncounties <- length(unique(dat_df$GEOID))
+  
+  s_long <- s %>%
+    pivot_longer(cols=all_of(1:ncounties),values_to="value",names_to="s")
+  
+  s_long$county_idx <- rep(1:ncounties,nrow(s))
+  s_long <- s_long %>% select(-s) %>% arrange(county_idx)
+
+  s_long$tot_idx <- rep(1:(max(s_long$samp_idx)*max(s_long$chain_idx)),max(s_long$county_idx))
+
+  if(spatial){
+    # est_cov_sc <- sapply(1:length(s_long),function(s){
+    #   sapply(1:length(beta_cov),function(i){
+    #     beta_cov[i]*anom_cov$sc + s_long$value
+    #   })
+    # })
+    
+    est_cov_sc <- array(NA,dim=c(nrow(anom_cov),length(beta_cov),length(unique(s_long$county_idx))))
+ 
+    if(scale_cov_name!="crp.nfsp.sc"){
+      for(i in 1:nrow(s_long)){
+        est_cov_sc[,s_long$tot_idx[i],s_long$county_idx[i]] <- 
+          beta_cov[s_long$tot_idx[i]]*anom_cov$sc + s_long$value[i]
+      }
+    } else {
+      for(i in 1:nrow(s_long)){
+        est_cov_sc[,s_long$tot_idx[i],s_long$county_idx[i]] <- 
+          beta_crp[s_long$tot_idx[i]]*crp_cov$sc +
+          beta_nfsp[s_long$tot_idx[i]]*nfsp_cov$sc +
+          beta_cov[s_long$tot_idx[i]]*anom_cov$sc + s_long$value[i] 
+      }
+    }
+
+  } else {
+    beta_int <- beta_long %>% filter(cov=="Intercept") %>% select(value)
+    beta_int <- unlist(c(beta_int),use.names = F)
+    
+    est_cov_sc <- sapply(1:length(beta_cov),function(i){
+      beta_int[i] + beta_cov[i]*anom_cov$sc
+      # rnorm(nrow(anom_cov),beta_int[i] + beta_cov[i]*anom_cov$sc,sqrt(1/tau[i]))
+
+    })
+  }
+  
+  quant_lci <- function(i){quantile(i,probs=0.025)}
+  quant_uci <- function(i){quantile(i,probs=0.975)}
+  
+  est_cov_sc[,1:10,1]
+  
+  mn = apply(est_cov_sc,c(1,3),mean)
+  md = apply(est_cov_sc,c(1,3),median)
+  lci=apply(est_cov_sc,c(1,3),quant_lci)
+  uci=apply(est_cov_sc,c(1,3),quant_uci)
+  
+  mn_long <- mn %>% as_data_frame() %>% 
+    mutate(cov_sc=anom_cov$sc,
+           cov_bt=anom_cov$bt) %>% 
+    pivot_longer(cols=all_of(1:length(unique(s_long$county_idx))),
+                 values_to="mn",names_to="county_idx")
+  
+  md_long <- md %>% as_data_frame() %>% 
+    mutate(cov_sc=anom_cov$sc,
+           cov_bt=anom_cov$bt) %>% 
+    pivot_longer(cols=all_of(1:length(unique(s_long$county_idx))),
+                 values_to="md",names_to="county_idx")
+  
+  lci_long <- lci %>% as_data_frame() %>% 
+    mutate(cov_sc=anom_cov$sc,
+           cov_bt=anom_cov$bt) %>% 
+    pivot_longer(cols=all_of(1:length(unique(s_long$county_idx))),
+                 values_to="lci",names_to="county_idx")
+  
+  uci_long <- uci %>% as_data_frame() %>% 
+    mutate(cov_sc=anom_cov$sc,
+           cov_bt=anom_cov$bt) %>% 
+    pivot_longer(cols=all_of(1:length(unique(s_long$county_idx))),
+                 values_to="uci",names_to="county_idx")
+  
+  est_cov_sum <- mn_long%>% left_join(md_long) %>% left_join(lci_long) %>% left_join(uci_long)
+  # est_cov_sum <- cbind.data.frame(anom_cov,est_cov_sum)
+  return(est_cov_sum)
+}
